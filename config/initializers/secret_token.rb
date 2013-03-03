399e1c17865cf31f15e728a8db8aecceece7504a
Clive <chenrick@kabam.com>
2013-03-02 20:39:19 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryEngine::Application.config.secret_token = '0125d9d0ea4aa425378c0d16d87a15029edfce2d2d66746d3e05379f9cf22ad6c47b93bc973161b9851348e1ff04d6dfd9d9ab2343f250e3d7beb286ff04f180'
